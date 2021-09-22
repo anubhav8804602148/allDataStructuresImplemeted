effc7fc0d6366963f08156105f3d9ce82a69c5ff
Anubhav Sharma <anubhav8804602148@gmail.com>
2021-09-22 15:24:25 +0530

    Tree done

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,7 +1,20 @@
 package dataStructure;
 
-public class BinaryTree extends Tree{
+import java.util.HashMap;
 
+public class BinaryTree extends Tree{
+	HashMap<TreeNode, Integer> hm;
+	public TreeNode balanceTree() {
+		hm = new HashMap<TreeNode, Integer>();
+		updateDepth(root, 0);
+		
+		return root;
+	}
+	public void updateDepth(TreeNode node, int depth) {
+		hm.put(node, depth);
+		if(node.left!=null) updateDepth(node.left, depth+1);
+		if(node.right!=null) updateDepth(node.right, depth+1);
+	}
 	@Override
 	public TreeNode add(int data) {
 		Stack<TreeNode> stack = new Stack<>(TreeNode.class);
@@ -12,6 +25,9 @@ public class BinaryTree extends Tree{
 		stack.push(root);
 		while(stack.getSize()>0) {
 			TreeNode curr = stack.pop();
+			if(curr.data==data) {
+				System.err.println("Duplicate value " + data + " at "+curr.toString());
+			}
 			if(curr.left==null && curr.right==null) {
 				if(curr.data>data) {
 					curr.left = new TreeNode();
@@ -22,9 +38,6 @@ public class BinaryTree extends Tree{
 					curr.right = new TreeNode();
 					curr.right.data = data;
 					return root;
-				}
-				else {
-					System.err.println("Duplicate value");
 				}
 			}
 			else if(curr.data>data) {
--- Controller.java
+++ Controller.java
@@ -1,11 +1,14 @@
 package dataStructure;
 
+import java.util.HashMap;
+import java.util.Random;
 
 public class Controller {
 	public static void main(String args[]) {
 		BinaryTree tree = new BinaryTree();
-		for(int i=0;i<10;i++) {
-			tree.add(i);
+		Random ran = new Random();
+		for(int i=1;i<=20;i++) {
+			tree.add(ran.nextInt(20));
 		}
 		tree.printTree("PRE");
 		System.out.println();
@@ -14,5 +17,10 @@ public class Controller {
 		tree.printTree("IN");
 		System.out.println();
 		tree.printTree("LEVEL");
+		tree.balanceTree();
+		HashMap<TreeNode, Integer> hm = tree.hm;
+		for(TreeNode tn : hm.keySet()) {
+			System.out.println(tn.data + " has depth " + hm.get(tn));
+		}
 	}
 }
